19df49da4833cb595cb894ba4af28310200a0ffe
marekwos-ifx <mwos@ifxpayments.com>
2024-04-04 10:24:37 +0200

    Quartz sync: Apr 4, 2024, 10:24 AM

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -54,7 +54,7 @@ export const Latex: QuartzTransformerPlugin<Options> = (opts?: Options) => {
           ],
           js: [
             {
-              src: "/assets/tikzjax.js",
+              src: "https://event15.github.io/metacybernetyka/assets/tikzjax.js",
               loadTime: "afterDOMReady",
               contentType: "external",
             },
